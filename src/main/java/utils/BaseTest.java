5f818a1bf96867a4e56c3e2277331a48aa457529
Constantin <costivscosty@yahoo.com>
2022-04-21 19:15:13 +0100

    seventh

--- src/main/java/utils/BaseTest.java
+++ src/main/java/utils/BaseTest.java
@@ -6,40 +6,75 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
 
-    public WebDriver driver;
+    WebDriver driver;
 
+    @Parameters({"browser"})
     @BeforeClass
-    public void setUp(){
-        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver100.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.addArguments("--headless");
+    public void setUp(String browser) throws MalformedURLException {
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver100");
 
-        driver = new ChromeDriver(options);
+
+        ChromeOptions option = new ChromeOptions();
+        option.addArguments("--headless");
+        option.addArguments("start-maximized");
+
+        FirefoxBinary firefoxBinary = new FirefoxBinary();
+        firefoxBinary.addCommandLineOptions("--headless");
+        FirefoxOptions foption = new FirefoxOptions();
+        foption.setBinary(firefoxBinary);
+
+        if(browser != "" && browser != null) {
+
+            if(browser.equalsIgnoreCase("chrome")) {
+                driver =  new ChromeDriver(option);
+
+            }else if(browser.equalsIgnoreCase("firefox")) {
+                driver =  new FirefoxDriver(foption);
+
+            }
+
+        }else {
+
+            driver =  new ChromeDriver(option);
+
+
+        }
+
+
+
+
+
         driver.manage().window().maximize();
-
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.manage().window().setSize(new Dimension(1440,900));
+
+        driver.manage().window().setSize(new Dimension(1440, 900));
+
         driver.get("https://keybooks.ro/");
-        //driver.get("https://the-internet.herokuapp.com/javascript_alerts");
-        //driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
     }
+
+
+
     @AfterClass
     public void tearDown() throws InterruptedException {
-        Thread.sleep(10000);
+        Thread.sleep(5000);
         driver.quit();
     }
 
@@ -52,9 +87,14 @@ public class BaseTest {
             TakesScreenshot poza = (TakesScreenshot) driver;
             File screenshot = poza.getScreenshotAs(OutputType.FILE);
             String timestamp =  new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss").format(new Date());
-           // Files.copy(screenshot, new File("screenshots/"+ result.getName() + timestamp + ".png"));
+            //Files.copy(screenshot, new File("screenshots/"+ result.getName() + timestamp + ".png"));
 
         }
+
+
+
+
+
 
     }
 
